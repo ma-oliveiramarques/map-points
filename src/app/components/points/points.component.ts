cb0c6cfa6df781ea7902558fcbf8479634838f80
Mariana Marques <ma_oliveiramarques@hotmail.com>
2023-01-04 23:42:33 +0000

    mocked points organized

--- src/app/components/points/points.component.ts
+++ src/app/components/points/points.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IPoint } from 'src/app/interfaces/point';
 import { PointsService } from 'src/app/services/points.service';
 import { SnackBarService } from 'src/app/services/snackbar.service';
+import { mockedPoints } from 'src/app/data/mocked-points';
 
 @Component({
   selector: 'app-points',
@@ -13,6 +14,7 @@ export class PointsComponent {
   pointsInserted: IPoint[];
   pointIdToDelete: string;
   pointIdToGet: string;
+  mockedPoints: IPoint[];
 
   constructor(
     private pointsService: PointsService,
@@ -21,47 +23,19 @@ export class PointsComponent {
     this.pointsInserted = [];
     this.pointIdToDelete = '';
     this.pointIdToGet = '';
+    this.mockedPoints= mockedPoints;
+    
   }
 
-
+// const mockedPoints: IPoint[] = 
   async createPoint(): Promise<void> {
-    const mockedPoints: IPoint[] = [
-      {
-        id: "1",
-        lat: -8.62597171350884,
-        long: 40.86669099337182,
-        name: "Ovar, Portugal",
-        color: "#191C7F"
-      }, 
-      {
-        id: "2",
-        lat: -3.704953086665833,
-        long: 40.42741629677429,
-        name: "Madrid, Espanha",
-        color: "#8B0000"
-      },
-      {
-        id: "3",
-        lat: -77.79439451189849,
-        long: 34.22011161652749,
-        name: "Wrightsville Beach, USA",
-        color: "#FF5500"
-      },
-      {
-        id: "4",
-        lat: 34.832266435574674,
-        long: -19.733360773134624,
-        name: "Cidade da Beira, Moçambique",
-        color: "#3C0781"
-      }
-    ];
-
 
     try {
-      const nextPoint: IPoint = mockedPoints.filter((p: IPoint) => !this.pointsInserted.some((pi: IPoint) => pi.id === p.id))[0];
+      const nextPoint: IPoint = this.mockedPoints.filter((p: IPoint) => !this.pointsInserted.some((pi: IPoint) => pi.id === p.id))[0];
 
       await this.pointsService.insertPoint(nextPoint);
       this.pointsInserted.push(nextPoint);
+      this.snackBarService.show("Point created successfully", "Close");
     }
     catch (err) {
       this.snackBarService.show('One error occurred', 'Close')
@@ -75,6 +49,7 @@ export class PointsComponent {
     }
 
     await this.pointsService.deletePoint(this.pointIdToDelete);
+    this.snackBarService.show('Point deleted successfully', 'Close');
   }
 
   async getPoint(): Promise<void> {
@@ -85,6 +60,22 @@ export class PointsComponent {
 
     const myPoint = await this.pointsService.getPoint(this.pointIdToGet);
     console.log(myPoint);
+    
+    // Create a DOM element
+    const div = document.getElementById('show-point');
+    const width = 600;
+    const height = 300;
+    const p = document.createElement('p');
+    p.innerHTML = `Point ID: ${this.pointIdToGet}` + '<br>' 
+                  + `Name: ${this.mockedPoints[myPoint - 1].name}` + '<br>'
+                  + `Longitude: ${this.mockedPoints[myPoint - 1].long}` + '<br>'
+                  + `Latitude: ${this.mockedPoints[myPoint - 1].lat}`;
+    div?.appendChild(p);
+  
+    div.style.backgroundColor = this.mockedPoints[this.pointIdToGet - 1].color;
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.backgroundSize = '100%';
   }
 
   async getPointsList(): Promise<void> {
---
+++ src/app/data/mocked-points.ts
@@ -0,0 +1,32 @@
+import { IPoint } from "../interfaces/point"
+
+export const mockedPoints: IPoint[] = [
+  {
+    id: "1",
+    lat: -8.62597171350884,
+    long: 40.86669099337182,
+    name: "Ovar, Portugal",
+    color: "#191C7F"
+  }, 
+  {
+    id: "2",
+    lat: -3.704953086665833,
+    long: 40.42741629677429,
+    name: "Madrid, Espanha",
+    color: "#8B0000"
+  },
+  {
+    id: "3",
+    lat: -77.79439451189849,
+    long: 34.22011161652749,
+    name: "Wrightsville Beach, USA",
+    color: "#FF5500"
+  },
+  {
+    id: "4",
+    lat: 34.832266435574674,
+    long: -19.733360773134624,
+    name: "Cidade da Beira, Moçambique",
+    color: "#3C0781"
+  }
+];
